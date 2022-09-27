d49e376fb73fadd5980b525fd61c2842bfee72f2
shahira-sadat <shahira.sadat1@gmail.com>
2022-09-27 11:43:56 +0430

    Create Teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,18 @@
-require "./person.rb"
+require_relative 'person'
 
 class Teacher < Person
-  def initialize(name, age = "unknown", parent_permission = true, specialization)
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
+  attr_accessor :specialization
+
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     @specialization = specialization
+    super(
+      age: age,
+      name: name,
+      parent_permission: parent_permission
+    )
   end
 
   def can_use_services?
-      true
+    true
   end
 end
-
-@specialization
