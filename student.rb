8cffb08abc6935d873acf700f6f00fea66f746b1
shahira-sadat <shahira.sadat1@gmail.com>
2022-09-29 19:12:44 +0430

    Add association to students

--- student.rb
+++ student.rb
@@ -1,11 +1,18 @@
 require_relative 'person'
 # Class Student inherites person
 class Student < Person
+  attr_reader :classroom
   attr_accessor :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    self.classroom = classroom
     @classroom = classroom
     super(age, name, parent_permission)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
   end
 
   def play_hooky
